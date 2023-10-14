06b72a59bd889ff90173b2a63f97a64afda4ac93
samwanyua <samexample8@gmail.com>
2023-10-14 22:23:37 +0300

    Variables

--- comments.c
+++ comments.c
@@ -3,6 +3,13 @@ int main(){
     
     //This is a comment. It will be ignored by the compiler
     printf("Hello world!");
+     
+     /* This 
+        is 
+        a 
+        multiline
+        commment
+        */
 
     return 0;
 }
---
+++ variables.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+
+int x; // declaration
+x = 23; // Initialization
+int y = 53434; //declaration + initialization
+
+
+int age = 54; //integer
+char grade = 'A'; //character
+float gpa = 4.23; //foating point number
+char name [] = "Onesmus"; //Array of characters
+
+printf("Hello %s\n", name);
+printf("You are %d years old\n", age);
+printf("Your average grade is %c.\n", grade);
+printf("Your gpa is %f \n", gpa);
+
+
+
+
+return 0;
+}
