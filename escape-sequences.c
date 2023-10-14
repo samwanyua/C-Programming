37a87fd3a98124ea5b4503187a5924b661522b30
samwanyua <samexample8@gmail.com>
2023-10-14 21:48:39 +0300

    Escape sequence

---
+++ escape-sequences.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+
+    printf("I love pizza\n"); // \n is a new line escape sequence
+    printf("\"One man can change the world.\" Abraham L."); // \" Double quotes
+
+    /* 
+    \n -    New line 
+    \" -    Double quotes
+    \' -    Single quote
+    \\ -    Backslash
+    \t -    Tab
+    \b -    Backspace
+    
+    */
+
+    return 0;
+}
