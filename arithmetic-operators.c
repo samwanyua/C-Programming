a97927247bda9ae1d12aab8d24ba7a872256860e
samwanyua <samexample8@gmail.com>
2023-10-16 13:09:32 +0300

    Arithmetic Operators

---
+++ arithmetic-operators.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+    /*
+    ARITHMETIC OPERATORS
+
+    + addition
+    - subtraction
+    * multiplication
+    / division
+    ++ increment
+    -- decrement
+    % modulus (gives the remainder)
+    
+    */
+
+   int x = 5;
+   int y = 2; 
+
+   //int z = x + y; // addition
+   //int z = x - y; // subtraction
+   //int z = x * y; //  multiplication
+   //float z = x / (float) y; // I converted y to be a float
+   //int z = x % y; //modulus
+
+   x ++; //increment
+   y--;  //decrement
+
+    
+   //printf("%f\n",z);
+   //printf("%d\n",z);
+   printf("%d\n",x);
+   printf("%d\n",y);
+
+   return 0;
+}
