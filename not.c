3712bbba584d9a7f57eb1488db10b34f13905957
samwanyua <samexample8@gmail.com>
2023-10-17 15:06:33 +0300

    Not ! Logical operator

---
+++ not.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    // Logical operator = ! (NOT) reverses the state of the condition
+    bool sunny = false;
+    
+
+    if(!sunny ==1 ){
+        printf("\nThe weather is bad!\n");
+    }
+    else{
+        printf("\nThe weather is good!\n");
+    }
+    return 0;
+}
